4d469288111ff57ca1ac44519f0d5e0917125331
chibuisi <enyia21@gmail.com>
2020-05-19 21:31:46 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,25 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :id, :name, :grade
 
+  def initialize(id=nil, name, grade)
+    # self.drop_table
+    # if self.id
+    #   students = self
+    #   DB[:conn].execute("DROP TABLE students")
+    # end
+
+    @id= id
+    @name = name
+    @grade = grade
+  end
+
+  def self.drop_table 
+    sql = <<-SQL 
+          DROP TABLE 
+          IF EXISTS students
+          SQL
+    DB[:conn].execute(sql)
+  end
 end
